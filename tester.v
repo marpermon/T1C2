7453d444c00d3664477d8362fd23c006621787f7
yo <yo@jammy.myguest.virtualbox.org>
2024-04-11 11:36:43 -0600

    prueba 5 incorporada

--- tester.v
+++ tester.v
@@ -23,19 +23,17 @@ parameter medio_T = 5;
     
     #5 Reset = 1; //5. reseteamos la máquina
     #10 Reset = 0; //15.
-    //*funcionamiento normal*
+    //*Prueba 1: funcionamiento normal*
     #5 Vehiculo = 1; //llega vehiculo
     #15 {enterPin,Pin}  = 9'b100000000+Pin_correcto;
     #10 enterPin = 0;
-    #25 {Vehiculo, Termino} = 2'b01; //si la señal de vehiculo se apaga, es logico de que la que termino de entrar tambien se encienda al mismo tiempo
+    #20 {Vehiculo, Termino} = 2'b01; //si la señal de vehiculo se apaga, es logico de que la que termino de entrar tambien se encienda al mismo tiempo
     #10 Termino = 0; //se apaga termino, dura sòlo un ciclo de relog
     //volvemos al estado cerrado
-    //*pin incorrecto +de 3 veces*
-    #10 Vehiculo = 1; //llega vehiculo
+    //*Prueba 2: ingreso de pin incorrecto 3 o más veces*
+    #15 Vehiculo = 1; //llega vehiculo
     #5 {enterPin,Pin}  = 9'b111111111; //introduce pin incorrecto
     #10 enterPin = 0;
-    //*probamos que no pase nada si cambia el pin aunque no esté prendido enter*
-    #10 Pin = Pin_0;
     #10 {enterPin,Pin}  = 9'b111111111; //introduce pin incorrecto por segunda vez
     #10 enterPin = 0;
     #10 enterPin = 1; //introduce pin incorrecto por tercera vez
@@ -47,15 +45,33 @@ parameter medio_T = 5;
     #10 {Vehiculo, Termino} = 2'b01; //si la señal de vehiculo se apaga, es logico de que la que termino de entrar tambien se encienda al mismo tiempo
     #10 Termino = 0; //se apaga termino, dura sòlo un ciclo de relog
      //volvemos al estado cerrado
-    //*menos de dos equivocaciones* 
-    #5 Vehiculo = 1; //llega nuevo vehiculo
-    #15 {enterPin,Pin}  = 9'b111111111; //introduce pin incorrecto sòlo una vez
+    //Prueba 3: *ingreso de pin incorrecto menos de 3 veces* 
+    #10 Vehiculo = 1; //llega nuevo vehiculo
+    #10 {enterPin,Pin}  = 9'b111111111; //introduce pin incorrecto sòlo una vez
     #10 enterPin = 0;
     #10 {enterPin,Pin}  = 9'b100000000+Pin_correcto;
     #10 enterPin = 0;
-    //*bloqueo por dos señales al mismo tiempo*
+    //Prueba 4: *Alarma de bloqueo*
     #10 {Vehiculo, Termino} = 2'b11; //si la señal de vehiculo se enciende al mismo tiempo que la señal termino, vamos al estado bloqueo
     #10 Termino = 0; //se apaga termino, dura sòlo un ciclo de relog
+    #10 {enterPin,Pin}  = 9'b111111111; //introduce pin incorrecto
+    #10 enterPin = 0; 
+    #10 {enterPin,Pin}  = 9'b100000000+Pin_correcto;
+    #10 enterPin = 0; //se vuelve a abrir la compuerta
+    //Prueba 5: *Botón enter*
+    #10 Reset = 1; //5. reseteamos la máquina
+    #10 Reset = 0; //15.
+    #10 Vehiculo = 1; //llega vehiculo
+    #10 Pin = Pin_0;//pin incorrecto
+    #20 Pin = Pin_correcto;//pin correcto
+    #20 enterPin = 1;
+    #10 enterPin = 0;//vamos al estado abierto
+    #10 {Vehiculo, Termino} = 2'b11; //bloqueamos la compuerta
+    #10 Termino = 0;
+    #10 Pin = Pin_0;//pin incorrecto
+    #20 Pin = Pin_correcto;//pin correcto
+    #20 enterPin = 1;
+    #10 enterPin = 0;
     #40 $finish;
   end
 
